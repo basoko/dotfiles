6dda522035e3befc80cac8c4bc4e12b60c60aab0
David Basoko <basoko@gmail.com>
2013-10-30 08:15:36 +0100

    Added xmobar configuration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,8 +10,15 @@ myModMask     = mod4Mask -- Win key or Super_L
 myBorderWidth = 2
 
 main = do
+   xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc.hs"
    xmonad $ defaultConfig
-     { terminal    = myTerminal
-     , modMask     = myModMask
-     , borderWidth = myBorderWidth
+     { manageHook = manageDocks <+> manageHook defaultConfig
+       , layoutHook = avoidStruts  $  layoutHook defaultConfig
+       , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+       , terminal    = myTerminal
+       , modMask     = myModMask -- Rebind Mod to the Windows key
+       , borderWidth = myBorderWidth
      }
