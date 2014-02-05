71a4ec582272644b7cb5586aa3c23587045a6645
David Basoko <basoko@gmail.com>
2014-02-05 14:06:15 +0100

    Added additional keys to manage multimedia actions

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.SetWMName
+import Graphics.X11.ExtraTypes.XF86
 
 myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
@@ -23,4 +24,16 @@ main = do
        , terminal    = myTerminal
        , modMask     = myModMask -- Rebind Mod to the Windows key
        , borderWidth = myBorderWidth
-     }
+     }  `additionalKeys`
+         [
+        ((mod4Mask .|. shiftMask, xK_l), spawn "slock")
+      -- Media Keys
+      , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2-")
+      , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2+")
+      , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle")
+      -- Spotify
+      , ((0, xF86XK_AudioPlay          ), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+      , ((0, xF86XK_AudioStop          ), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop")
+      , ((0, xF86XK_Back               ), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
+      , ((0, xF86XK_Forward            ), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
+      ]
